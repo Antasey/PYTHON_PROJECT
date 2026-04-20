350d5103ddae53d01cfa1a67d1a423260656bc36
Antasey <104510167+Antasey@users.noreply.github.com>
2026-04-20 13:15:53 +0100

    PyNotes

---
+++ datatypes.py
@@ -0,0 +1,12 @@
+# You have the Grace of God
+# Datatypes 
+# numbers
+# 1. int - integers [whole numbers]
+# 2. float - floating point [decimal point]
+
+# Text 
+# str - strings - "[a very intelligent boy]"
+# logical/ conditional
+# True and False - both capitals.
+# boolean 
+# none
